dd6af930209c0e980ae9366dda72199e05b62573
Raghav Patnecha <raghavpatnecha06@gmail.com>
2021-07-11 13:41:53 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool=mysql.createPool({
     host:'localhost',
     user:'root',
     database:'headth2',
-    password:''
+    password:'headth'
 });
 //comment added
 module.exports=pool.promise();
